b4518bb7648c5a4e7ee9b80b21fc177022d4accc
Glitch (glitch-blank-website) <none>
2022-03-17 13:34:10 +0000

    🍄💰 Checkpoint ./style.css:43111395/739 ./index.html:43111395/25 ./script.js:43111395/1185

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0; 
+var gamePlaying = false;
 
-console.log("Hello, world!");
+function startGame(){
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+function stopGame(){
+  gamePlaying = false;
+  
+  // swap the Start and Stop buttons
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
+}
+
