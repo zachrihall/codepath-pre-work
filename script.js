b14e27a4e8d7b66b1a4ab6e1e5d9ad4ce5663eb2
Glitch (glitch-blank-website) <none>
2022-03-17 14:09:05 +0000

    👰👳 Checkpoint ./script.js:43111395/2435

--- script.js
+++ script.js
@@ -110,9 +110,29 @@ function winGame(){
 
 function guess(btn){
   console.log("user guessed: " + btn);
+  
   if(!gamePlaying){
     return;
   }
   
-  // add game logic here
+  if(pattern[guessCounter] == btn){
+    //Guess was correct!
+    if(guessCounter == progress){
+      if(progress == pattern.length - 1){
+        //GAME OVER: WIN!
+        winGame();
+      }else{
+        //Pattern correct. Add next segment
+        progress++;
+        playClueSequence();
+      }
+    }else{
+      //so far so good... check the next guess
+      guessCounter++;
+    }
+  }else{
+    //Guess was incorrect
+    //GAME OVER: LOSE!
+    loseGame();
+  }
 }
